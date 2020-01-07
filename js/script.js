1c575c0d54da12c1c7249205640898edc80c3bb9
Decline-of-Mind <L.h.gotink@gmail.com>
2020-01-07 11:48:51 +0000

    Expanded on click to button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
  //
  // add your jQuery code here
-
-
+$("#button1").on("mouseenter", function() {
+    $("#button1").removeClass("makeRed").addClass("makeBorder");
+    });
+$("#button1").on("mouseleave", function() {
+    $("#button1").removeClass("makeBorder").addClass("makeRed");
+    });
+$("#button1").on("click", function() {
+    $(".card").siblings('#para1').slideToggle(400);
+    });
 }); 
